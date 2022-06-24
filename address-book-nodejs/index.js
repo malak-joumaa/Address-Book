89d041e63adca5a963a429553e41a8223d17272c
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-06-24 08:29:47 +0300

    Adding user table with api and middleware

--- address-book-nodejs/index.js
+++ address-book-nodejs/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRouter = require("./src");
+
+// mongodDB online -> use Atlas
+
+const DB_CONNECT = process.env.DB_CONNECT || "";
+mongoose.connect(DB_CONNECT, () => console.log("connected to db"));
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", userRouter);
+
+app.listen(5000, () => console.log("Server running"));
---
+++ address-book-nodejs/src/index.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const { get, register, login } = require("./controller/user");
+const router = Router();
+const testMiddleware = require("../middleware/test");
+
+router.get("/", testMiddleware(), (req, res) => get(req, res));
+router.post("/auth/register", register);
+router.post("/auth/login", login);
+
+module.exports = router;
