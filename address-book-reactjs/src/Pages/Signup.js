4dee48f78e3cbb30be9d089d7eb9c071e62e20d7
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-06-25 01:43:01 +0300

    Front-end: Adding Sign Up form

--- address-book-reactjs/src/Pages/Signup.js
+++ address-book-reactjs/src/Pages/Signup.js
@@ -1,7 +1,40 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return <div>Sign</div>;
+  return (
+    <div>
+      <div className="container">
+        <form className="user-form">
+          <div className="box">
+            <h2 className="title">Sign In</h2>
+            <label className="lbl">Name</label>
+            <br />
+            <input type="text" id="name" className="txtbox" name="text" />
+            <br />
+            <label className="lbl">Email</label>
+            <br />
+            <input type="email" id="email" className="txtbox" name="text" />
+            <br />
+            <label className="lbl">Password</label>
+            <br />
+            <input
+              type="password"
+              id="password"
+              className="txtbox"
+              name="text"
+            />
+            <br />
+            <button className="btn">Sign up</button>
+            <br />
+            <span>
+              Already have an account? <Link to="/">Sign In</Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
