36e2f4c4dd086267d7a2232f31b2da4b4a5d4740
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-06-24 23:51:03 +0300

    Front-end: Addign Sign in form

--- address-book-reactjs/src/App.js
+++ address-book-reactjs/src/App.js
@@ -1,6 +1,49 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
-function App() {}
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div>
+                <div className="container">
+                  <form className="user-form">
+                    <div className="box">
+                      <h2 className="title">Sign In</h2>
+                      <label className="lbl">Email</label>
+                      <br />
+                      <input
+                        type="email"
+                        id="email"
+                        className="txtbox"
+                        name="text"
+                      />
+                      <br />
+                      <label className="lbl">Password</label>
+                      <br />
+                      <input
+                        type="password"
+                        id="password"
+                        className="txtbox"
+                        name="text"
+                      />
+                      <br />
+                      <button className="btn">Sign in</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </>
+          }
+        ></Route>
+        <Route path="/sign-up"></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
