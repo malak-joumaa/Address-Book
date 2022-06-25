736a47398b019257bb3aa78c4534aa7951cb0cc6
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-06-25 21:21:45 +0300

    Adding add contact form

--- address-book-reactjs/src/Pages/AddContact.js
+++ address-book-reactjs/src/Pages/AddContact.js
@@ -1,6 +1,71 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
 
-const AddContact = () => {};
+const AddContact = () => {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [value, setValue] = useState();
+  console.log(fname);
+  console.log(lname);
+  console.log(email);
+  console.log(value);
+
+  return (
+    <div>
+      <h1>Add Contact</h1>
+      <form>
+        <label>First Name</label>
+        <br />
+        <input
+          type="text"
+          id="lname"
+          className="txtbox"
+          value={fname}
+          onChange={(e) => setFname(e.target.value)}
+        />
+        <br />
+        <label>Last Name</label>
+        <br />
+        <input
+          type="text"
+          id="fname"
+          className="txtbox"
+          value={lname}
+          onChange={(e) => setLname(e.target.value)}
+        />
+        <br />
+        <label>Email</label>
+        <br />
+        <input
+          type="email"
+          id="email"
+          className="txtbox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <label>Relationship Status</label>
+        <br />
+        <input type="radio" name="status" />
+        Single
+        <input type="radio" name="status" />
+        Married
+        <br />
+        <label>Phone number</label>
+        <br />
+        <PhoneInput
+          placeholder="Enter phone number"
+          value={value}
+          onChange={setValue}
+        />
+        <br />
+        <label>Location</label>
+      </form>
+    </div>
+  );
+};
 
 export default AddContact;
